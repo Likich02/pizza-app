f50c3ff95d713f8c4539bacd5712f2e2e06e63b6
Lukich <lukovskii02@mail.ru>
2024-03-20 13:03:50 +0700

    Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import "./App.css";
+import { MouseEvent, useState } from "react";
+
 import Button from "./components/Button/Button";
 
 function App() {
+  const [counter, setCounter] = useState<number>(0);
+
+  const addCounter = (e: MouseEvent) => {
+    console.log(e);
+  };
+
   return (
     <>
-      <Button onClick={() => console.log()}>Кнопка</Button>
+      <Button onClick={addCounter}>Кнопка</Button>
     </>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,14 @@
-import { FC } from "react";
-import "./Button.module.css";
+import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import cn from "classnames";
-
-export const ButtonAlt: FC<ButtonProps> = ({
-  children,
-  className,
-  ...props
-}) => {
-  return (
-    <>
-      <button className={cn("button accent", className)} {...props}>
-        {children}
-      </button>
-    </>
-  );
-};
 
 function Button({ children, className, ...props }: ButtonProps) {
   console.log("Button");
   return (
-    <button className={cn("button accent", className)} {...props}>
+    <button
+      className={cn(styles["button"], styles["accent"], className)}
+      {...props}
+    >
       {children}
     </button>
   );
