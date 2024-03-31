5b5b344d0c8ca21b4112436d9b8d081de3c70dab
Likich02 <124595507+Likich02@users.noreply.github.com>
2024-03-31 20:18:26 +0700

    Add React router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { MouseEvent, useState } from "react";
-
+import { Route, Routes } from "react-router-dom";
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
+import { Cart } from "./pages/Cart/Cart";
+import { Menu } from "./pages/Menu/Menu";
+import { Error } from "./pages/Error/Error";
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -17,6 +20,15 @@ function App() {
         Кнопка
       </Button>
       <Input placeholder="Email" />
+      <div>
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
