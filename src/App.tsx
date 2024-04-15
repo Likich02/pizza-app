150efa1e7dac75d4e7c0573608b2f27da8c5346e
Likich02 <124595507+Likich02@users.noreply.github.com>
2024-04-15 18:39:59 +0700

    add Link & Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,10 @@ import { MouseEvent, useState } from "react";
 
 import Button from "./components/Button/Button";
 import Input from "./components/Input/Input";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Cart } from "./pages/Cart/Cart";
 import { Menu } from "./pages/Menu/Menu";
 import { Error } from "./pages/Error/Error";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "*",
-    element: <Error />,
-  },
-]);
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -36,11 +21,6 @@ function App() {
         Кнопка
       </Button>
       <Input placeholder="Email" />
-      <div>
-        <a href="/">Меню</a>
-        <a href="/cart">Корзина</a>
-      </div>
-      <RouterProvider router={router} />
     </>
   );
 }
