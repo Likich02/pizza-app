eff95849ac4f192012df7eafa0835fe4fbd233d3
Likich02 <124595507+Likich02@users.noreply.github.com>
2024-04-16 16:41:15 +0700

    useLocation

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,8 +1,15 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import styles from "./Layout.module.css";
 import Button from "../../components/Button/Button";
+import { useEffect } from "react";
+import cn from "classnames";
 
 export function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
   return (
     <div className={styles["layout"]}>
       <div className={styles["sidebar"]}>
@@ -12,7 +19,12 @@ export function Layout() {
           <div className={styles["email"]}>lukovskii02@mail.ru</div>
         </div>
         <div className={styles["menu"]}>
-          <Link to="/" className={styles["link"]}>
+          <Link
+            to="/"
+            className={cn(styles["link"], {
+              [styles.active]: location.pathname === "/",
+            })}
+          >
             <img
               src="/menu-icon.svg"
               alt="Иконка меню"
