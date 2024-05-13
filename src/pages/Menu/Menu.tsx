71588aa5256436f0c18e4d09d5c78574a8e4f65c
Likich02 <124595507+Likich02@users.noreply.github.com>
2024-05-14 01:05:12 +0700

    add cartProduct

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Headling from "../../components/Headlink/Headling";
+import ProductCart from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
 import styles from "./Menu.module.css";
 
@@ -8,6 +9,16 @@ export function Menu() {
       <div className={styles["head"]}>
         <Headling>Меню</Headling>
         <Search placeholder="Введите блюдо или состав" />
+      </div>
+      <div>
+        <ProductCart
+          id={1}
+          title="Наслаждение"
+          description="Салями, руккола, помидоры, оливки"
+          rating={4.5}
+          price={300}
+          image="/product-demo.png"
+        />
       </div>
     </>
   );
