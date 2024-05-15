8a6814fd198792b2d2ef743f1b2b8c31cd66e661
Likich02 <124595507+Likich02@users.noreply.github.com>
2024-05-15 12:43:06 +0700

    loader&errorElement

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,11 @@ import {
 } from "react-router-dom";
 import { Menu } from "./pages/Menu/Menu.tsx";
 import { Cart } from "./pages/Cart/Cart.tsx";
-import { Error } from "./pages/Error/Error.tsx";
+import { Error as ErrorPage } from "./pages/Error/Error.tsx";
 import { Layout } from "./layout/Menu/Layout.tsx";
 import { Product } from "./pages/Product/Product.tsx";
+import axios from "axios";
+import { PREFIX } from "./helpers/API.ts";
 
 const router = createBrowserRouter([
   {
@@ -29,13 +31,23 @@ const router = createBrowserRouter([
       {
         path: "/product/:id",
         element: <Product />,
+        errorElement: <>Ошибка</>,
+        loader: async ({ params }) => {
+          await new Promise<void>((resolve) => {
+            setTimeout(() => {
+              resolve();
+            }, 2000);
+          });
+          const { data } = await axios.get(`${PREFIX}/products/${params.id}`);
+          return data;
+        },
       },
     ],
   },
 
   {
     path: "*",
-    element: <Error />,
+    element: <ErrorPage />,
   },
 ]);
 
